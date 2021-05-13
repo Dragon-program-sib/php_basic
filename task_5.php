8b880541ec758d22c625e41d1e8c6d043f1a4854
Dragon <dragon.program.sib@gmail.com>
2021-05-13 22:41:00 +0700

    Update

--- task_5.php
+++ task_5.php
@@ -4,11 +4,15 @@
 
 $a = 1;
 $b = 2;
+echo "a = $a, b = $b<br><br>";
 
 // 1 вариант.
 $a = ($a + $b);
+echo "a = $a, b = $b<br>";
 $b = $a - $b;
+echo "a = $a, b = $b<br><br>";
 $a = $a - $b;
+echo "a = $a, b = $b";
 
 // 2 вариант.
-($a = $a + $b) & ($b = $a - $b) & ($a = $a - $b);
+$a = $a + $b & $b = $a - $b & $a = $a - $b; // $a = 2, $b = 1
