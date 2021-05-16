e59f76aea32a27fec69d9af9e557b38013714afa
Dragon <dragon.program.sib@gmail.com>
2021-05-17 04:27:37 +0700

    Added about.php, menu.php, layout.php

--- site/index.php
+++ site/index.php
@@ -2,4 +2,13 @@
 
 /* 5. Собрать страницу с меню и контентом, зарендерить как минимум 2 подшаблона через renderTemplate. ВАЖНОЕ */
 
+function renderTemplate($page, $content = '')
+{
+    ob_start();
+    include 'templates/' . $page . ".php";
+    return ob_get_clean();
+}
 
+$about = renderTemplate('about');
+
+echo renderTemplate('layouts/layout', $about);
