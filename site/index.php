40259457772ebb95a8834287a1485a475b9f2c2e
Dragon <dragon.program.sib@gmail.com>
2021-05-17 20:50:25 +0700

    Update index.php

--- site/index.php
+++ site/index.php
@@ -2,15 +2,14 @@
 
 /* 5. Собрать страницу с меню и контентом, зарендерить как минимум 2 подшаблона через renderTemplate. ВАЖНОЕ */
 
-function renderTemplate($page, $content = '')
+function renderTemplate($page, $content = '', $menu = '')
 {
     ob_start();
     include 'templates/' . $page . ".php";
     return ob_get_clean();
 }
 
-$menu = renderTemplate('menu');
 $main = renderTemplate('main');
+$menu = renderTemplate('menu');
 
-echo renderTemplate('layouts/layout', $menu);
-echo renderTemplate('layouts/layout', $main);
+echo renderTemplate('layouts/layout', $main, $menu);
